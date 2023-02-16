c3bccabe627bdaa48fda2c390e39b4672a38a067
Uche Patrick <uchepatric04@gmail.com>
2023-02-16 15:23:08 -0800

    fix error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - print the string in the put function
+ * return: 0
+ */
 
-int main ()
+int main (void)
 {
-	puts("Programming is like building a multilingual puzzle /n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
