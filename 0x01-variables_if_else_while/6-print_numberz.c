34b083a5ecc5c3334e8a790325f26ce0364d5635
Uche Patrick <uchepatric04@gmail.com>
2023-02-20 14:54:25 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - code block
+ * Description: a program that prints numbers from zero - ten
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
