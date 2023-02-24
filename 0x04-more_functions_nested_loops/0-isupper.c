f076044cfa169fb368469227342e003b05de4ef4
Uche Patrick <uchepatric04@gmail.com>
2023-02-23 16:51:33 -0800

    fix error

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,15 @@
 /**
  * _isupper - checks for uppercase
+ * @c: variable for alphabet
  * Return: 1 or 0
  */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
-	{
 		return (1);
-	}
 	else 
-	{
 		return (0);
-	}
 }
 
 
