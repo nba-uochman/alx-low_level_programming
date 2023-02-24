a7fe3562cec5e411f2ef287b943e20189af36e58
Uche Patrick <uchepatric04@gmail.com>
2023-02-23 16:47:56 -0800

    first commit

---
+++ 0x03-debugging/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+	int i;
+
+i = 0;
+while (i < 10)
+{
+    i++;
+    printf("%d", i / 2);
+}
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/**
+ * _isupper - checks for uppercase
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
+
+
