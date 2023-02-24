9b94626aa2a13d54f9be9fb080a5f9301c5424d3
Uche Patrick <uchepatric04@gmail.com>
2023-02-23 19:19:22 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,14 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if(i != 1)
+		if (i != 1)
 			printf(" ");
 
 		 if (i % 3 == 0 && i % 5 == 0)
 			 printf("FizzBuzz");
 		 else if (i % 3 == 0)
 			printf("Fizz");
-		 else if(i % 5 == 0)
+		 else if (i % 5 == 0)
 			 printf("Buzz");
 		 else 
 			 printf("%d", i);
